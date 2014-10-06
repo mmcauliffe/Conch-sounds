4cddad182e25345ee4776b9443f3acc997cd5ac3
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-10-05 22:52:01 -0700

    Base class work

--- acousticsim/representations/base.py
+++ acousticsim/representations/base.py
@@ -1,4 +1,17 @@
 
 
 class Representation(object):
-    pass
+    _duration = None
+    _sr = None
+    _filepath = None
+    _rep = None
+
+    def window(self, win_len, time_step):
+        pass
+
+    def segment(self,thresh):
+        pass
+
+    @property
+    def shape(self):
+        return _rep.shape
