b723d48dbe0d05435955590f07ba71a4c9bde574
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2015-03-10 16:43:21 -0700

    ampenv filter coeffs for testing

--- tests/test_ampenv.py
+++ tests/test_ampenv.py
@@ -16,12 +16,23 @@ TEST_DIR = os.path.abspath('tests/data')
 
 filenames = [os.path.splitext(x)[0] for x in os.listdir(TEST_DIR) if x.endswith('.wav')]
 
+
+
 class EnvelopeTest(unittest.TestCase):
     def setUp(self):
         self.num_bands = 4
         self.freq_lims = (80,7800)
 
+    def test_filter_coeffs(self):
+        b = [[0.0011,0, -0.0022, 0, 0.0011],
+            [0.0097, 0, -0.0194, 0, 0.0097],
+            [ 0.0741,0,-0.1482 , 0 , 0.0741],
+            [0.4629, 0 ,-0.9258 , 0  ,  0.4629]]
 
+        a = [[1.0000, -3.8988 ,  5.7072, -3.7175  ,  0.9092],
+            [1.0000  ,-3.6459 ,  5.0421 , -3.1369 ,   0.7415],
+            [1.0000 ,-2.6224  , 2.8816 , -1.5936  ,  0.3927],
+            [1.0000  , 1.1957 , -0.1477 ,  -0.1166  ,  0.2388],]
 
     def test_envelope_gen(self):
         return
