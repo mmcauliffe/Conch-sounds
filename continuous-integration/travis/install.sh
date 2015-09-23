1c49ee2894bf324fe662fe5e933fe757764d84ec
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2015-09-23 15:50:11 -0400

    Revert to older version of praat

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 set -e
 #check to see if miniconda folder is empty
-if [ ! -d "$HOME/miniconda/envs/test-environment" ]; then
+if [ ! -d "$HOME/miniconda" ]; then
   wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
   ./miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
   conda config --set always_yes yes --set changeps1 no
-  conda config --add channels dsdale24
   conda update -q conda
   conda info -a
   conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION atlas numpy scipy pytest scikit-learn networkx
@@ -19,12 +18,15 @@ fi
 
 if [ ! -d "$HOME/downloads/praat" ]; then
   cd $HOME/downloads
-  latestVer=$(curl -s 'http://www.fon.hum.uva.nl/praat/download_linux.html' |
-   grep -Eo 'praat[0-9]+_linux64\.tar\.gz' | head -1)
+  #FOR WHEN TRAVIS UPDATES TO A NEWER UBUNTU
+  #latestVer=$(curl -s 'http://www.fon.hum.uva.nl/praat/download_linux.html' |
+  # grep -Eo 'praat[0-9]+_linux64\.tar\.gz' | head -1)
 
   # Download.
-  curl "http://www.fon.hum.uva.nl/praat/${latestVer}" > praat-latest.tar.gz
-  tar -zxvf praat-latest.tar.gz
+  #curl "http://www.fon.hum.uva.nl/praat/${latestVer}" > praat-latest.tar.gz
+  #tar -zxvf praat-latest.tar.gz
+  wget http://www.fon.hum.uva.nl/praat/old/5412/praat5412_linux64.tar.gz
+  tar -zxvf praat5412_linux64.tar.gz
 else
   echo "Praat already installed."
 fi
