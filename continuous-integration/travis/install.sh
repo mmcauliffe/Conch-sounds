93bae763e4f12ebf53271b8ecc88f354985200b3
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2015-09-23 15:55:10 -0400

    Update miniconda installation

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 #check to see if miniconda folder is empty
-if [ ! -d "$HOME/miniconda" ]; then
+if [ ! -d "$HOME/miniconda/envs/test-environment" ]; then
   wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
-  ./miniconda.sh -b -p $HOME/miniconda
+  ./miniconda.sh -b -f -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
   conda config --set always_yes yes --set changeps1 no
   conda update -q conda
