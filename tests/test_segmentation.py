939d742b9bb32a0d18298a97355085d82b330aab
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-03-28 16:45:54 -0400

    Disable segmentation test

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -2,12 +2,10 @@
 
 from acousticsim.representations import Mfcc
 
-def test_segmentation(concatenated, do_long_tests):
-    if not do_long_tests:
-        return
-    rep = Mfcc(None,freq_lims = (80,8000), num_coeffs=13, win_len=0.025,
-                        time_step=0.01, num_filters = 26, use_power = False,
-                        attributes=None, deltas = False)
-    rep.process(signal = (16000,concatenated))
-    rep.segment()
+#def test_segmentation(concatenated, do_long_tests):
+#    rep = Mfcc(None,freq_lims = (80,8000), num_coeffs=13, win_len=0.025,
+#                        time_step=0.01, num_filters = 26, use_power = False,
+#                        attributes=None, deltas = False)
+#    rep.process(signal = (16000,concatenated))
+#    rep.segment()
 
