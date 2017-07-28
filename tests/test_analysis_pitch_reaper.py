3ee6a568ba4aa71320c46e80ff4cd301251a307d
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-07-28 16:26:43 -0400

    Fix reaper test

--- tests/test_analysis_pitch_reaper.py
+++ tests/test_analysis_pitch_reaper.py
@@ -1,14 +1,11 @@
 import pytest
 from acousticsim.analysis.pitch.reaper import file_to_pitch_reaper
+from statistics import mean
 
-from numpy.testing import assert_array_almost_equal
 
-
-@pytest.mark.xfail
 def test_reaper(noise_path, y_path, reaperpath):
     rep = file_to_pitch_reaper(noise_path, reaper_path=reaperpath, time_step=0.01, min_pitch=75, max_pitch=600)
-    assert (rep.to_array().mean() == -1)
+    assert (mean(x['F0'] for x in rep.values()) == -1)
 
     rep = file_to_pitch_reaper(y_path, reaper_path=reaperpath, time_step=0.01, min_pitch=75, max_pitch=600)
-    print(rep.to_array())
-    assert (rep.to_array()[1:-1].mean() - 98.)
+    assert (mean(x['F0'] for x in rep.values())  - 98.)
