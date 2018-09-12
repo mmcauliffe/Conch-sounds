e799e5ec8199516474ee6eb374ac4e472a248b86
Michael Goodale <michael.goodale@mail.mcgill.ca>
2018-09-12 12:54:42 -0400

    got arguments passing without error

--- tests/test_analysis_autovot.py
+++ tests/test_analysis_autovot.py
@@ -3,15 +3,15 @@ import librosa
 from statistics import mean
 import wave
 import pytest
-from conch.analysis.segments import FileSegment, SignalSegment
-from conch.exceptions import FunctionMismatch
+from conch.analysis.segments import SegmentMapping
+from conch import analyze_segments
 
 
 def test_pitch_praat(autovot_filenames, autovot_markings):
     mapping = SegmentMapping()
     for filename in autovot_filenames:
-        with wave.open(path,'r') as f: 
-            length = f.getnframes() / float(f.getframerate)
+        with wave.open(filename,'r') as f: 
+            length = f.getnframes() / float(f.getframerate())
         mapping.add_file_segment(filename, 0, length, channel=0, vot_marks=autovot_markings[filename])
     func = AutoVOTAnalysisFunction()
-    output = analyze_segments(mapping, reaper_func, multiprocessing=False)
+    output = analyze_segments(mapping, func, multiprocessing=False)
