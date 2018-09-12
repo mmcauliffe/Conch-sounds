2b95605a454de86f2004839a8d92a9291e4b0306
Michael Goodale <michael.goodale@mail.mcgill.ca>
2018-09-12 14:14:47 -0400

    Got extgrid generation working

--- conch/analysis/autovot.py
+++ conch/analysis/autovot.py
@@ -10,11 +10,15 @@ class MeasureVOTPretrained(object):
         if autovot_binaries_path is None:
             self.autovot_binaries_path = 'auto_vot_train.py'
 
-    def __call__(self, *args, **kwargs):
-        file_path, begin, end = args[:3]
+    def __call__(self, segment):
+        file_path = segment["file_path"]
+        begin = segment["begin"]
+        end = segment["end"]
+        vot_marks = segment["vot_marks"]
         grid = textgrid.TextGrid(maxTime=end)
         vot_tier = textgrid.IntervalTier(name="vot", maxTime=end)
-        vot_tier.add(begin, end, "vot")
+        for (vot_begin, vot_end) in vot_marks:
+            vot_tier.add(vot_begin, vot_end, "vot")
         grid.append(vot_tier)
 
 
@@ -24,3 +28,4 @@ class AutoVOTAnalysisFunction(BaseAnalysisFunction):
         self._function = MeasureVOTPretrained()
         self.requires_file = True
         self.uses_segments = True
+        self.requires_segment_as_arg = True
