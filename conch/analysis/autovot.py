3f4830ae1e4809eeccd6073e4532ee0882202eea
Michael Goodale <michael.goodale@mail.mcgill.ca>
2018-09-12 14:41:39 -0400

    Added subprocess

--- conch/analysis/autovot.py
+++ conch/analysis/autovot.py
@@ -6,9 +6,11 @@ import os
 import tempfile
 
 class MeasureVOTPretrained(object):
-    def __init__(self, autovot_binaries_path=None):
+    def __init__(self, autovot_binaries_path=None, classifier_to_use=None):
         if autovot_binaries_path is None:
-            self.autovot_binaries_path = 'auto_vot_train.py'
+            self.autovot_binaries_path = '/home/michael/Honours-Thesis/autovot/autovot/bin/auto_vot_decode.py'
+        if classifier_to_use is None:
+            self.classifier_to_use = '/home/michael/Honours-Thesis/autovot/experiments/models/bb_jasa.classifier.pos'
 
     def __call__(self, segment):
         file_path = segment["file_path"]
@@ -16,14 +18,27 @@ class MeasureVOTPretrained(object):
         end = segment["end"]
         vot_marks = segment["vot_marks"]
         grid = textgrid.TextGrid(maxTime=end)
-        vot_tier = textgrid.IntervalTier(name="vot", maxTime=end)
+        vot_tier = textgrid.IntervalTier(name='vot', maxTime=end)
         for (vot_begin, vot_end) in vot_marks:
-            vot_tier.add(vot_begin, vot_end, "vot")
+            vot_tier.add(vot_begin, vot_end, 'vot')
         grid.append(vot_tier)
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            grid_path = "{}/file.TextGrid".format(tmpdirname)
+            csv_path = "{}/file.csv".format(tmpdirname)
+            wav_filenames = "{}/wavs.txt".format(tmpdirname)
+            textgrid_filenames = "{}/textgrids.txt".format(tmpdirname)
 
+            with open(wav_filenames, 'w') as f:
+                f.write("{}\n".format(file_path))
+
+            with open(textgrid_filenames, 'w') as f:
+                f.write("{}\n".format(grid_path))
+
+            grid.write(grid_path)
+            subprocess.run([self.autovot_binaries_path, wav_filenames, textgrid_filenames, self.classifier_to_use, '--vot_tier', 'vot', '--vot_mark', 'vot', '--csv_file', csv_path])
 
 class AutoVOTAnalysisFunction(BaseAnalysisFunction):
-    def __init__(self, autovot_binaries_path=None, arguments=None):
+    def __init__(self, autovot_binaries_path=None, classifier_to_use=None, arguments=None):
         super(AutoVOTAnalysisFunction, self).__init__()
         self._function = MeasureVOTPretrained()
         self.requires_file = True
