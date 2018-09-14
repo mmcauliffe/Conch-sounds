5071acc9442626b68411aac39a41f0e143374708
Michael Goodale <michael.goodale@mail.mcgill.ca>
2018-09-14 14:08:14 -0400

    Added vot min max parametres to make voiced test set runnable

--- conch/analysis/autovot.py
+++ conch/analysis/autovot.py
@@ -6,11 +6,13 @@ import os
 import tempfile
 
 class MeasureVOTPretrained(object):
-    def __init__(self, autovot_binaries_path=None, classifier_to_use=None):
+    def __init__(self, autovot_binaries_path=None, classifier_to_use=None, min_vot_length=15, max_vot_length=250):
         if autovot_binaries_path is None:
             self.autovot_binaries_path = '/home/michael/Honours-Thesis/autovot/autovot/bin/auto_vot_decode.py'
         if classifier_to_use is None:
             self.classifier_to_use = '/home/michael/Honours-Thesis/autovot/experiments/models/bb_jasa.classifier'
+        self.min_vot_length = min_vot_length
+        self.max_vot_length = max_vot_length
 
     def __call__(self, segment):
         file_path = segment["file_path"]
@@ -35,19 +37,16 @@ class MeasureVOTPretrained(object):
                 f.write("{}\n".format(grid_path))
 
             grid.write(grid_path)
-            subprocess.run([self.autovot_binaries_path, wav_filenames, textgrid_filenames, self.classifier_to_use, '--vot_tier', 'vot', '--vot_mark', 'vot', '--csv_file', csv_path])
+            subprocess.run([self.autovot_binaries_path, wav_filenames, textgrid_filenames, self.classifier_to_use, '--vot_tier', 'vot', '--vot_mark', 'vot', '--csv_file', csv_path, "--min_vot_length", str(self.min_vot_length), "--max_vot_length", str(self.max_vot_length)])
             with open(csv_path, "r") as f: 
-                x = f.readline()
-                y = f.readline()
-                print(x)
-                print(y)
-                #_, time, vot, confidence = y.split(',')
-            #print(f"{time}, {vot}, {confidence}")
+                f.readline()
+                _, time, vot, confidence = f.readline().split(',')
+            return (time, vot)
 
 class AutoVOTAnalysisFunction(BaseAnalysisFunction):
-    def __init__(self, autovot_binaries_path=None, classifier_to_use=None, arguments=None):
+    def __init__(self, autovot_binaries_path=None, classifier_to_use=None, min_vot_length=15, max_vot_length=250, arguments=None):
         super(AutoVOTAnalysisFunction, self).__init__()
-        self._function = MeasureVOTPretrained()
+        self._function = MeasureVOTPretrained(autovot_binaries_path=autovot_binaries_path, classifier_to_use=classifier_to_use, min_vot_length=min_vot_length, max_vot_length=max_vot_length)
         self.requires_file = True
         self.uses_segments = True
         self.requires_segment_as_arg = True
--- tests/test_analysis_autovot.py
+++ tests/test_analysis_autovot.py
@@ -13,5 +13,5 @@ def test_pitch_praat(autovot_filenames, autovot_markings):
         with wave.open(filename,'r') as f: 
             length = f.getnframes() / float(f.getframerate())
         mapping.add_file_segment(filename, 0, length, channel=0, vot_marks=autovot_markings[filename])
-    func = AutoVOTAnalysisFunction()
+    func = AutoVOTAnalysisFunction(min_vot_length=5, max_vot_length=100)
     output = analyze_segments(mapping, func, multiprocessing=False)
